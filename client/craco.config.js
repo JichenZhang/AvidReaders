75d47fbeb54ead82662ff88520e9182ec519d5ff
JichenZhang <jzhang877@wisc.edu>
2021-08-04 01:10:41 +0800

    finished Dashboard page, fixed selection height and color

---
+++ client/craco.config.js
@@ -0,0 +1,27 @@
+const CracoLessPlugin = require('craco-less');
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            modifyVars: { '@table-bg': 'rgba(0,0,0,0)',
+                '@table-border-color': 'rgba(0,0,0,0)',
+                '@select-background':'rgba(0,0,0,0)',
+                '@select-border-color': 'rgba(0,0,0,0)',
+                '@table-header-bg':'#76bddc',
+                '@table-header-color':'#FFFFFF',
+                '@font-size-base':'30px',
+                '@select-dropdown-height':'50px',
+                '@select-single-item-height':'50px',
+                '@select-multiple-item-height-lg': '50px'
+            },
+            javascriptEnabled: true,
+          },
+        },
+      },
+    },
+  ],
+};
